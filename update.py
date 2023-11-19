b1c72e305b0067736620e9b0c0542dec3867f5e6
Chris McComb <ccmcc2012@gmail.com>
2023-11-19 15:30:08 -0500

    Update update.py

--- update.py
+++ update.py
@@ -112,12 +112,14 @@ fig = plotly.express.scatter(
 
 fig.update_xaxes(
     visible=False,
-    autorange=False
+    autorange=False,
+    range=[numpy.min(pca_embeddings[:,0])*1.05, numpy.max(pca_embeddings[:,0])*1.05]
     )
 fig.update_yaxes(
     visible=False,
     scaleanchor="x",
     scaleratio=1,
+    range=[numpy.min(pca_embeddings[:,1])*1.05, numpy.max(pca_embeddings[:,1])*1.05]
     )
 
 fig.update_layout(
@@ -129,7 +131,6 @@ fig.update_layout(
 
 fig.show(config={
     'displaylogo': False,
-    # 'modeBarButtonsToRemove': ['select', 'lasso2d']
     }
 )
 
