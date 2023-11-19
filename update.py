45a09c26d6336d7b40f43e465da6a08f279d33d5
Chris McComb <ccmcc2012@gmail.com>
2023-11-19 13:40:21 -0500

    Lots of updates

--- update.py
+++ update.py
@@ -14,69 +14,68 @@ import sys
 
 # List of tuples of faculty names and google scholar IDs
 faculty_in_department = [
-    ("Farimani", "aH52nxkAAAAJ"),
-    ("Bedillion", "UIS_G1YAAAAJ"),
-    ("Bergbreiter", "SU0c5P0AAAAJ"),
-    # ("Beuth", None),
-    ("Bolmin", "YM2HSekAAAAJ"),
-    ("Cagan", "VtINcewAAAAJ"),
-    ("de Boer", "P1OWCkAAAAAJ"),
-    # ("Gomez", None),
+    ("Farimani",         "aH52nxkAAAAJ"),
+    ("Bedillion",        "UIS_G1YAAAAJ"),
+    ("Bergbreiter",      "SU0c5P0AAAAJ"),
+    ("Beuth",            None),
+    ("Bolmin",           "YM2HSekAAAAJ"),
+    ("Cagan",            "VtINcewAAAAJ"),
+    ("de Boer",          "P1OWCkAAAAAJ"),
+    ("Gomez",            None),
     ("Grande Gutiérrez", "HzsuNvEAAAAJ"),
-    # ("Haidar", None),
-    ("Halilaj", "Fr9Vhe4AAAAJ"),
-    ("Jayan", "NNSeBe4AAAAJ"),
-    ("Johnson", "GN5Mc3UAAAAJ"),
-    ("Jones", "xnyNe2EAAAAJ"),
-    ("Kang", "jcgAPTYAAAAJ"),
-    ("Kara", "_X2eOeQAAAAJ"),
-    ("LeDuc", "sLEB9iYAAAAJ"),
-    ("Litster", "7zUqi1sAAAAJ"),
-    ("Majidi", "1LyndUsAAAAJ"),
-    ("Malen", "U8r6ir4AAAAJ"),
-    ("McComb", "0P9w_S0AAAAJ"),
-    ("McGaughey", "HmNtygkAAAAJ"),
-    ("Michalek", "92jyX2EAAAAJ"),
-    ("Narra", "LxF27NYAAAAJ"),
-    ("Ozdoganlar", "F3UFKWMAAAAJ"),
-    ("Panat", "y1-WJhYAAAAJ"),
-    ("Presto", "4iYpwt8AAAAJ"),
-    ("Quinn", "2b0ZjdsAAAAJ"),
-    ("Rabin", "INTSXtcAAAAJ"),
-    ("Shen", "9KtTt28AAAAJ"),
-    ("Shimada", "pgcK25AAAAAJ"),
-    ("Singh", "lViJAdQAAAAJ"),
-    ("Steif", "NVHAIw4AAAAJ"),
-    ("Sullivan", "wXSgFr4AAAAJ"),
-    ("Taylor", "eDzzGUQAAAAJ"),
-    ("Tucker", "8N6uFIkAAAAJ"),
-    ("Weber", "7iFu3a4AAAAJ"),
-    ("Webster-Wood", "jW3whJYAAAAJ"),
-    ("Whitefoot", "3W2VK5sAAAAJ"),
-    ("Zhang", "dzHYBKcAAAAJ"),
-    ("Zhao", "z7tPc9IAAAAJ"),
+    ("Haidar",           None),
+    ("Halilaj",          "Fr9Vhe4AAAAJ"),
+    ("Jayan",            "NNSeBe4AAAAJ"),
+    ("Johnson",          "GN5Mc3UAAAAJ"),
+    ("Jones",            "xnyNe2EAAAAJ"),
+    ("Kang",             "jcgAPTYAAAAJ"),
+    ("Kara",             "_X2eOeQAAAAJ"),
+    ("LeDuc",            "sLEB9iYAAAAJ"),
+    ("Litster",          "7zUqi1sAAAAJ"),
+    ("Majidi",           "1LyndUsAAAAJ"),
+    ("Malen",            "U8r6ir4AAAAJ"),
+    ("McComb",           "0P9w_S0AAAAJ"),
+    ("McGaughey",        "HmNtygkAAAAJ"),
+    ("Michalek",         "92jyX2EAAAAJ"),
+    ("Narra",            "LxF27NYAAAAJ"),
+    ("Ozdoganlar",       "F3UFKWMAAAAJ"),
+    ("Panat",            "y1-WJhYAAAAJ"),
+    ("Presto",           "4iYpwt8AAAAJ"),
+    ("Quinn",            "2b0ZjdsAAAAJ"),
+    ("Rabin",            "INTSXtcAAAAJ"),
+    ("Shen",             "9KtTt28AAAAJ"),
+    ("Shimada",          "pgcK25AAAAAJ"),
+    ("Singh",            "lViJAdQAAAAJ"),
+    ("Steif",            "NVHAIw4AAAAJ"),
+    ("Sullivan",         "wXSgFr4AAAAJ"),
+    ("Taylor",           "eDzzGUQAAAAJ"),
+    ("Tucker",           "8N6uFIkAAAAJ"),
+    ("Weber",            "7iFu3a4AAAAJ"),
+    ("Webster-Wood",     "jW3whJYAAAAJ"),
+    ("Whitefoot",        "3W2VK5sAAAAJ"),
+    ("Zhang",            "dzHYBKcAAAAJ"),
+    ("Zhao",             "z7tPc9IAAAAJ"),
 ]
 
 
 # Function to save faculty publications to a json file
-def save_publications_to_json(faculty: tuple[str, str]):
-    print(faculty[0])
+def save_publications_to_json(faculty_tuple: tuple[str, str]):
+    print(faculty_tuple[0])
     faculty_pubs = []
-    if faculty[1]:
-        author = scholarly.fill(scholarly.search_author_id(faculty[1]))
+    if faculty_tuple[1]:
+        author = scholarly.fill(scholarly.search_author_id(faculty_tuple[1]))
         for publication in author['publications']:
             faculty_pubs.append(publication['bib'])
-    with open(faculty[0] + '.json', 'w', encoding='utf-8') as f:
+    with open(faculty_tuple[0] + '.json', 'w', encoding='utf-8') as f:
         json.dump(faculty_pubs, f, ensure_ascii=False, indent=4)
-    return faculty_pubs
 
 
 # If this is a testing run instead of a full one, trim the faculty list
 n = len(faculty_in_department) if len(sys.argv) == 1 else int(sys.argv[1])
 
 # Get information for all faculty and save to json
-for f in faculty_in_department[:n]:
-    save_publications_to_json(f)
+for faculty in faculty_in_department[:n]:
+    save_publications_to_json(faculty)
 
 # Collect json files
 path_to_json = './'
