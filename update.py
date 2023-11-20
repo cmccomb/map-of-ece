ac8ee6f33b86f982cdae23e87a91965251ed576d
Chris McComb <ccmcc2012@gmail.com>
2023-11-19 19:53:26 -0500

    Formatting

--- update.py
+++ update.py
@@ -1,60 +1,59 @@
-import json
-import matplotlib.pyplot
-import matplotlib.colors
-import numpy
-import os
-import pandas
-import plotly.express
-import plotly.graph_objects
-from scholarly import scholarly
-import sentence_transformers
-import sklearn.decomposition
-import sklearn.manifold
-import sys
+import json                     # for saving and parsing json files
+import matplotlib.pyplot        # for convering rgb to hex
+import matplotlib.colors        # for getting pretty colors
+import numpy                    # for generic operations
+import os                       # for searching for json files
+import pandas                   # dumping json into csv
+import plotly.express           # plotly
+from scholarly import scholarly # google scholar
+import sentence_transformers    # embeddings
+import sklearn.decomposition    # orient tsne
+import sklearn.manifold         # make a tsne
+import sys                      # for testing
 
 # List of faculty names and google scholar IDs
 faculty_in_department: list[tuple[str, str]] = [
-    ("Farimani", "aH52nxkAAAAJ"),
-    ("Bedillion", "UIS_G1YAAAAJ"),
-    ("Bergbreiter", "SU0c5P0AAAAJ"),
-    # ("Beuth", None),
-    ("Bolmin", "YM2HSekAAAAJ"),
-    ("Cagan", "VtINcewAAAAJ"),
-    ("de Boer", "P1OWCkAAAAAJ"),
-    # ("Gomez", None),
-    ("Grande Gutiérrez", "HzsuNvEAAAAJ"),
-    # ("Haidar", None),
-    ("Halilaj", "Fr9Vhe4AAAAJ"),
-    ("Jayan", "NNSeBe4AAAAJ"),
-    ("Johnson", "GN5Mc3UAAAAJ"),
-    ("Jones", "xnyNe2EAAAAJ"),
-    ("Kang", "jcgAPTYAAAAJ"),
-    ("Kara", "_X2eOeQAAAAJ"),
-    ("LeDuc", "sLEB9iYAAAAJ"),
-    ("Litster", "7zUqi1sAAAAJ"),
-    ("Majidi", "1LyndUsAAAAJ"),
-    ("Malen", "U8r6ir4AAAAJ"),
-    ("McComb", "0P9w_S0AAAAJ"),
-    ("McGaughey", "HmNtygkAAAAJ"),
-    ("Michalek", "92jyX2EAAAAJ"),
-    ("Narra", "LxF27NYAAAAJ"),
-    ("Ozdoganlar", "F3UFKWMAAAAJ"),
-    ("Panat", "y1-WJhYAAAAJ"),
-    ("Presto", "4iYpwt8AAAAJ"),
-    ("Quinn", "2b0ZjdsAAAAJ"),
-    ("Rabin", "INTSXtcAAAAJ"),
-    ("Shen", "9KtTt28AAAAJ"),
-    ("Shimada", "pgcK25AAAAAJ"),
-    ("Singh", "lViJAdQAAAAJ"),
-    ("Steif", "NVHAIw4AAAAJ"),
-    ("Sullivan", "wXSgFr4AAAAJ"),
-    ("Taylor", "eDzzGUQAAAAJ"),
-    ("Tucker", "8N6uFIkAAAAJ"),
-    ("Weber", "7iFu3a4AAAAJ"),
-    ("Webster-Wood", "jW3whJYAAAAJ"),
-    ("Whitefoot", "3W2VK5sAAAAJ"),
-    ("Zhang", "dzHYBKcAAAAJ"),
-    ("Zhao", "z7tPc9IAAAAJ"),
+    ("Farimani",            "aH52nxkAAAAJ"),
+    ("Bedillion",           "UIS_G1YAAAAJ"),
+    ("Bergbreiter",         "SU0c5P0AAAAJ"),
+    ("Beuth",               None),
+    ("Bolmin",              "YM2HSekAAAAJ"),
+    ("Cagan",               "VtINcewAAAAJ"),
+    ("de Boer",             "P1OWCkAAAAAJ"),
+    ("Gomez",               None),
+    ("Grande Gutiérrez",    "HzsuNvEAAAAJ"),
+    ("Haidar",              None),
+    ("Halilaj",             "Fr9Vhe4AAAAJ"),
+    ("Jayan",               "NNSeBe4AAAAJ"),
+    ("Johnson",             "GN5Mc3UAAAAJ"),
+    ("Jones",               "xnyNe2EAAAAJ"),
+    ("Kang",                "jcgAPTYAAAAJ"),
+    ("Kara",                "_X2eOeQAAAAJ"),
+    ("LeDuc",               "sLEB9iYAAAAJ"),
+    ("Litster",             "7zUqi1sAAAAJ"),
+    ("Majidi",              "1LyndUsAAAAJ"),
+    ("Malen",               "U8r6ir4AAAAJ"),
+    ("McComb",              "0P9w_S0AAAAJ"),
+    ("McGaughey",           "HmNtygkAAAAJ"),
+    ("Michalek",            "92jyX2EAAAAJ"),
+    ("Narra",               "LxF27NYAAAAJ"),
+    ("Ozdoganlar",          "F3UFKWMAAAAJ"),
+    ("Panat",               "y1-WJhYAAAAJ"),
+    ("Presto",              "4iYpwt8AAAAJ"),
+    ("Quinn",               "2b0ZjdsAAAAJ"),
+    ("Rabin",               "INTSXtcAAAAJ"),
+    ("Shen",                "9KtTt28AAAAJ"),
+    ("Shimada",             "pgcK25AAAAAJ"),
+    ("Singh",               "lViJAdQAAAAJ"),
+    ("Steif",               "NVHAIw4AAAAJ"),
+    ("Sullivan",            "wXSgFr4AAAAJ"),
+    ("Taylor",              "eDzzGUQAAAAJ"),
+    ("Tucker",              "8N6uFIkAAAAJ"),
+    ("Weber",               "7iFu3a4AAAAJ"),
+    ("Webster-Wood",        "jW3whJYAAAAJ"),
+    ("Whitefoot",           "3W2VK5sAAAAJ"),
+    ("Zhang",               "dzHYBKcAAAAJ"),
+    ("Zhao",                "z7tPc9IAAAAJ"),
 ]
 
 # Function to save publications from Google Scholar
