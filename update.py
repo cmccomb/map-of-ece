df1baf4919f8b8a0518e1585976db5323f05c333
Chris McComb <ccmcc2012@gmail.com>
2023-11-19 20:01:00 -0500

    Changing variable names

--- update.py
+++ update.py
@@ -78,8 +78,8 @@ def save_publications_to_json(faculty: tuple[str, str]) -> list[dict[str, str]]:
 #     faculty_in_department = faculty_in_department[:n]
 
 number_of_faculty: int = len(faculty_in_department) if len(sys.argv) == 1 else int(sys.argv[1])
-for faculty in faculty_in_department[:number_of_faculty]:
-    save_publications_to_json(faculty)
+for faculty_tuple in faculty_in_department[:number_of_faculty]:
+    save_publications_to_json(faculty_tuple)
 
 # Identify all the json files
 path_to_json = './'
