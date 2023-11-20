4decc0cea4bf2f6bc8342ba6a9695064350c1ac7
Chris McComb <ccmcc2012@gmail.com>
2023-11-19 19:58:34 -0500

    fixing bugs

--- update.py
+++ update.py
@@ -77,7 +77,7 @@ def save_publications_to_json(faculty: tuple[str, str]) -> list[dict[str, str]]:
 #     n = int(sys.argv[1])
 #     faculty_in_department = faculty_in_department[:n]
 
-number_of_faculty: int = len(faculty_in_department) if len(sys.argv == 1) else int(sys.argv[1])
+number_of_faculty: int = len(faculty_in_department) if len(sys.argv) == 1 else int(sys.argv[1])
 for faculty in faculty_in_department[:number_of_faculty]:
     save_publications_to_json(faculty)
 
