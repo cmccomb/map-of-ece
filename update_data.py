893126daba82537356addc0e7f1eb8b7e0c12e52
Chris McComb <ccmcc2012@gmail.com>
2023-11-25 17:32:04 -0500

    Cleanup and simplification

--- update_data.py
+++ update_data.py
@@ -3,21 +3,17 @@ from scholarly import scholarly     # for scraping data from Google Scholar
 import pandas                       # for reading data
 
 
-# Function to save publications from Google Scholar
-def save_publications_to_json(faculty: dict) -> None:
-    name = faculty['name']
-    google_scholar_id = faculty['id']
-    if not pandas.isnull(google_scholar_id):
-        author = scholarly.fill(scholarly.search_author_id(google_scholar_id))
-        faculty_pubs = [publication['bib'] for publication in author['publications']]
-        with open('data/' + name + '.json', 'w', encoding='utf-8') as f:
-            json.dump(sorted(faculty_pubs, key=lambda x: x['title']), f, ensure_ascii=False, indent=4)
-
-
 # List of faculty names and Google Scholar IDs
 faculty_in_department = pandas.read_csv("faculty.csv").to_dict('records')
 
 # Get publications for every faculty member
-for faculty_dict in faculty_in_department:
-    print(faculty_dict)
-    save_publications_to_json(faculty_dict)
+for faculty in faculty_in_department:
+    if not pandas.isnull(faculty['id']):    # make sure there is an ID in the dict
+        # Get and fill the info
+        author = scholarly.fill(scholarly.search_author_id(faculty['id']))
+        # Extract publications
+        faculty_pubs = [publication['bib'] for publication in author['publications']]
+        # Save to JSON
+        with open('data/' + faculty['name'] + '.json', 'w', encoding='utf-8') as f:
+            json.dump(sorted(faculty_pubs, key=lambda x: x['title']), f, ensure_ascii=False, indent=4)
+
